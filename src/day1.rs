1974583c30ae6d13ba5845f1b7e193efc1777a6d
Ralph Recto <recto.ralph@gmail.com>
2018-12-06 20:11:08 -0500

    Cleanup.

--- src/day1.rs
+++ src/day1.rs
@@ -1,16 +1,45 @@
 use fileutil;
+use std::collections::HashSet;
+
+fn pt1(lines: &Vec<String>) -> () {
+    let sum: i32 = lines
+        .iter()
+        .map(|x: &String| {
+            let n: i32 = x.parse().unwrap();
+            return n;
+        }).fold(0, |acc, x| acc + x);
+
+    println!("pt 1: {}", sum);
+}
+
+fn pt2(lines: &Vec<String>) -> () {
+    let mut seen: HashSet<i32> = HashSet::new();
+    let mut rolling: i32 = 0;
+    loop {
+        let mut triggered: bool = false;
+        for line in lines {
+            let num: i32 = line.parse().unwrap();
+            rolling += num;
+            if (seen.contains(&rolling)) {
+                println!("pt2: {}", rolling);
+                triggered = true;
+                break;
+            } else {
+                seen.insert(rolling);
+            }
+        }
+
+        if (triggered) {
+            break;
+        }
+    }
+}
 
 pub fn run() -> () {
     match fileutil::read_lines("./data/01.txt") {
         Ok(lines) => {
-            let sum: i32 = lines
-                .iter()
-                .map(|x: &String| {
-                    let n: i32 = x.parse().unwrap();
-                    return n;
-                }).fold(0, |acc, x| acc + x);
-
-            println!("{}", sum);
+            pt1(&lines);
+            pt2(&lines);
         }
         Err(e) => println!("{}", e)
     }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 mod day1;
-mod day2;
 mod fileutil;
 
 fn main() -> () {
-//    day1::run();
-    day2::run();
+    day1::run();
 }
