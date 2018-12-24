b62082ce44938a43fd111baf8b8ce6e32237d13e
Ralph Recto <recto.ralph@gmail.com>
2018-12-24 12:50:14 -0500

    Finished day 4, pt. 1.

--- src/day4.rs
+++ src/day4.rs
@@ -1,36 +1,31 @@
-extern crate chrono;
 use fileutil;
 use std::collections::HashMap;
-
-type datetime = chrono::DateTime<chrono::FixedOffset>;
 
 #[derive(Debug)]
 enum EventType {
     BeginShift(i32),
-    FallAsleep,
-    WakeUp
+    FallAsleep(i32),
+    WakeUp(i32)
 }
 
 #[derive(Debug)]
-struct Event {
-    timestamp: chrono::DateTime<chrono::offset::FixedOffset>,
+struct Event<'a> {
+    timestamp: &'a str,
     event_type: EventType
 }
 
 fn parse_line(line: &str) -> Event {
-    let timestamp = chrono::DateTime::parse_from_str(&format!("{}{}", &line[1..17], " +0000"),
-    "%Y-%m-%d %H:%M %z")
-        .unwrap();
-
+    let timestamp: &str = &line[1..17];
+    let mins: i32 = (&line[15..17]).parse().unwrap();
     let event_type_str: &str = &line[19..];
 
     let event_type = if event_type_str.starts_with("Guard") {
         let splits: Vec<&str> = event_type_str.split(" ").collect();
         EventType::BeginShift(splits[1][1..].parse::<i32>().unwrap())
     } else if event_type_str.starts_with("falls") {
-        EventType::FallAsleep
+        EventType::FallAsleep(mins)
     } else {
-        EventType::WakeUp
+        EventType::WakeUp(mins)
     };
 
     Event { timestamp, event_type }
@@ -45,7 +40,8 @@ pub fn run() -> () {
 
             chronological_events.sort_by_key(|e| e.timestamp);
 
-            let mut sleeps_by_id = HashMap::new();
+            // map of form (guard id -> list of pairs of begin/end sleep mins)
+            let mut sleeps_by_id: HashMap<i32, Vec<(i32, i32)>> = HashMap::new();
             let mut curr_id = Option::None;
             let mut begin_sleep = Option::None;
             for e in chronological_events {
@@ -53,34 +49,56 @@ pub fn run() -> () {
                     EventType::BeginShift(id) => {
                         curr_id = Option::Some(id);
                     }
-                    EventType::FallAsleep => {
-                        begin_sleep = Option::Some(e.timestamp);
+                    EventType::FallAsleep(sleep_min) => {
+                        begin_sleep = Option::Some(sleep_min);
                     }
-                    EventType::WakeUp => {
+                    EventType::WakeUp(wake_min) => {
                         sleeps_by_id.entry(curr_id.unwrap())
-                            .or_insert(Vec::<(datetime, datetime)>::new())
-                            .push((begin_sleep.unwrap(), e.timestamp));
+                            .or_insert(Vec::<(i32, i32)>::new())
+                            .push((begin_sleep.unwrap(), wake_min));
 
                         begin_sleep = Option::None;
                     }
                 }
             }
 
-            let max_id: i32 = sleeps_by_id
-                .iter()
-                .map(|(id, sleeps): (&i32, &Vec<(datetime, datetime)>)| {
-                    let sum: i64 = sleeps
-                        .iter()
-                        .map(|&(start_t, end_t)|
-                            start_t.signed_duration_since(end_t).num_minutes()
-                        ).sum();
+            // Task 1: find the guard who sleeps the most
+            let mut curr_max_id = None;
+            let mut curr_max_mins = 0;
+            for (id, sleeps) in &sleeps_by_id {
+                let sum: i32 = sleeps.iter()
+                    .map(|pair| pair.1 - pair.0)
+                    .sum();
 
-                    (id, sum)
-                }).max_by_key(|&(id, sum)| sum)
-                .map(|(id, sum)| id)
-                .unwrap();
+                if sum > curr_max_mins {
+                    curr_max_mins = sum;
+                    curr_max_id = Option::Some(id);
+                }
+            }
 
-            println!("{}", max_id);
+            println!("guard {} slept the most ({} mins)", curr_max_id.unwrap(), curr_max_mins);
+
+            let max_id_sleeps: &Vec<(i32, i32)> =
+                (&sleeps_by_id).get(curr_max_id.unwrap()).unwrap();
+
+            // Task 2: find the minute they sleep the most
+            let mut awake_count: [u32; 60] = [0; 60];
+            for &(start, end) in max_id_sleeps.iter() {
+                for min in start..end {
+                    awake_count[min as usize] += 1;
+                }
+            }
+
+            let mut max_idx = 0;
+            let mut max_idx_curr_count = 0;
+            for i in 0..awake_count.len() {
+                if awake_count[i] > max_idx_curr_count {
+                    max_idx = i;
+                    max_idx_curr_count = awake_count[i];
+                }
+            }
+
+            println!("max min asleep was {}", max_idx);
         }
         Err(e) => panic!(e)
     }
