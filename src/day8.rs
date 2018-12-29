57583443c09cb823ac45a920ce844f15061ded80
Ralph Recto <recto.ralph@gmail.com>
2018-12-29 01:35:18 -0500

    Day 8, pt. 2.

--- src/day8.rs
+++ src/day8.rs
@@ -18,6 +18,30 @@ fn metadata_sum(mut numlist: &mut LinkedList<i32>) -> i32 {
     children_sum + node_sum
 }
 
+fn value(mut numlist: &mut LinkedList<i32>) -> i32 {
+    let num_children = numlist.pop_front().unwrap();
+    let num_metadata = numlist.pop_front().unwrap();
+
+    let children_value: Vec<i32> = (0..num_children)
+        .map(|child| value(&mut numlist))
+        .collect();
+
+    let mut node_value = 0;
+    for _ in 0..num_metadata {
+        let metadata= numlist.pop_front().unwrap();
+
+        if num_children > 0 {
+            if metadata > 0 && metadata <= num_children {
+                node_value += children_value[(metadata - 1) as usize];
+            }
+        } else {
+            node_value += metadata
+        }
+    }
+
+    node_value
+}
+
 pub fn run() -> () {
     match fileutil::read_file("./data/08.txt") {
         Ok(s) => {
@@ -26,7 +50,10 @@ pub fn run() -> () {
                 .collect();
 
             println!("part 1");
-            println!("metadata sum: {:?}", metadata_sum(&mut numlist));
+            println!("metadata sum: {:?}", metadata_sum(&mut numlist.clone()));
+
+            println!("part 2");
+            println!("value: {:?}", value(&mut numlist.clone()));
         }
         Err(e) => {
             panic!(e);
