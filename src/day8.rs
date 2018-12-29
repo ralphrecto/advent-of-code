1c3f091a7920c088b10d0e844a76f0e5de5b6ef7
Ralph Recto <recto.ralph@gmail.com>
2018-12-29 13:39:02 -0500

    Removed data copies and linked lists for day 8 pt. 2

--- src/day8.rs
+++ src/day8.rs
@@ -1,41 +1,41 @@
 use fileutil;
-use std::collections::LinkedList;
+use std::slice::Iter;
 
-fn metadata_sum(mut numlist: &mut LinkedList<i32>) -> i32 {
-    let num_children = numlist.pop_front().unwrap();
-    let num_metadata = numlist.pop_front().unwrap();
+fn metadata_sum(mut numlist: &mut Iter<i32>) -> i32 {
+    let num_children = numlist.next().unwrap();
+    let num_metadata = numlist.next().unwrap();
 
     let mut children_sum = 0;
-    for _ in 0..num_children {
+    for _ in 0..*num_children {
         children_sum += metadata_sum(&mut numlist);
     }
 
     let mut node_sum = 0;
-    for _ in 0..num_metadata {
-        node_sum += numlist.pop_front().unwrap();
+    for _ in 0..*num_metadata {
+        node_sum += numlist.next().unwrap();
     }
 
     children_sum + node_sum
 }
 
-fn value(mut numlist: &mut LinkedList<i32>) -> i32 {
-    let num_children = numlist.pop_front().unwrap();
-    let num_metadata = numlist.pop_front().unwrap();
+fn value(mut numlist: &mut Iter<i32>) -> i32 {
+    let num_children = numlist.next().unwrap();
+    let num_metadata = numlist.next().unwrap();
 
-    let children_value: Vec<i32> = (0..num_children)
+    let children_value: Vec<i32> = (0..*num_children)
         .map(|child| value(&mut numlist))
         .collect();
 
     let mut node_value = 0;
-    for _ in 0..num_metadata {
-        let metadata= numlist.pop_front().unwrap();
+    for _ in 0..*num_metadata {
+        let metadata= numlist.next().unwrap();
 
-        if num_children > 0 {
-            if metadata > 0 && metadata <= num_children {
+        if *num_children > 0 {
+            if *metadata > 0 && *metadata <= *num_children {
                 node_value += children_value[(metadata - 1) as usize];
             }
         } else {
-            node_value += metadata
+            node_value += *metadata
         }
     }
 
@@ -45,15 +45,16 @@ fn value(mut numlist: &mut LinkedList<i32>) -> i32 {
 pub fn run() -> () {
     match fileutil::read_file("./data/08.txt") {
         Ok(s) => {
-            let mut numlist: LinkedList<i32> = s.split(" ")
+            let mut numlist: Vec<i32> = s.split(" ")
                 .map(|numstr| numstr.parse::<i32>().unwrap())
                 .collect();
+            let whole_slice: &[i32] = &numlist[..];
 
             println!("part 1");
-            println!("metadata sum: {:?}", metadata_sum(&mut numlist.clone()));
+            println!("metadata sum: {:?}", metadata_sum(&mut whole_slice.iter()));
 
             println!("part 2");
-            println!("value: {:?}", value(&mut numlist.clone()));
+            println!("value: {:?}", value(&mut whole_slice.iter()));
         }
         Err(e) => {
             panic!(e);
