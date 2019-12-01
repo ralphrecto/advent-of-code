972b019ed5a089fa485a5b59f359cce50b39df69
Ralph Recto <recto.ralph@gmail.com>
2019-12-01 13:23:42 -0500

    Day 1, part 1.

--- src/advent2019/day1.rs
+++ src/advent2019/day1.rs
@@ -1,11 +1,15 @@
 use fileutil;
 
-fn pt1(lines: &[String]) -> () {
-    println!("hi");
+fn pt1(lines: &Vec<String>) -> () {
+    let sum: i32 = lines.iter()
+        .map(|mass_str| (mass_str.parse::<i32>().unwrap() / 3) - 2)
+        .sum();
+
+    println!("{}", sum);
 }
 
 pub fn run() -> () {
-    match fileutil::read_lines("./data/2018/01.txt") {
+    match fileutil::read_lines("./data/2019/01.txt") {
         Ok(lines) => {
             pt1(&lines);
         }
