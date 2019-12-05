0ecf630f3dd335da3bd0d3543fc2a5a1161d49c4
Ralph Recto <recto.ralph@gmail.com>
2019-12-04 23:01:20 -0500

    Day 4, part 2.

--- src/advent2019/day04.rs
+++ src/advent2019/day04.rs
@@ -1,6 +1,6 @@
-fn pt1(low: i32, high: i32) -> () {
+pub fn run() -> () {
     let mut num_passwords = 0;
-    for i in low..=high {
+    for i in 284639..=748759 {
         // Assume i is a 6 digit number.            
         let d1 = i / 100000;
         let d2 = (i / 10000) % 10;
@@ -10,16 +10,16 @@ fn pt1(low: i32, high: i32) -> () {
         let d6 = i % 10;
         
         let monotone = d1 <= d2 && d2 <= d3 && d3 <= d4 && d4 <= d5 && d5 <= d6;
-        let has_adjacent = d1 == d2 || d2 == d3 || d3 == d4 || d4 == d5 || d5 == d6;
+        let adjacent_ok = (d1 == d2 && d2 != d3) ||
+            (d2 == d3 && d1 != d2 && d3 != d4) ||
+            (d3 == d4 && d2 != d3 && d4 != d5) ||
+            (d4 == d5 && d3 != d4 && d5 != d6) ||
+            (d5 == d6 && d4 != d5);
 
-        if monotone && has_adjacent {
+        if monotone && adjacent_ok {
             num_passwords += 1;
         }
     }
 
     println!("num passwords: {}", num_passwords);
-}
-
-pub fn run() -> () {
-    pt1(284639, 748759);
 }
