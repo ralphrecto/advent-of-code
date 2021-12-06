84e3376e9d04c5664b9f31e045e3bf10bd011f96
Ralph Recto <recto.ralph@gmail.com>
2021-12-05 23:00:19 -0500

    Finish day 5 part 1

---
+++ src/advent2021/day05.rs
@@ -0,0 +1,92 @@
+use std::collections::HashMap;
+
+use fileutil;
+
+#[derive(Clone,Copy,PartialEq,Eq,Hash,Debug)]
+struct Point {
+    x: i32,
+    y: i32
+}
+
+impl Point {
+    pub fn parse_point(point_str: &str) -> Point {
+        let mut splits = point_str.split(",");
+        
+        Point {
+            x: splits.next().and_then(|s| s.trim().parse::<i32>().ok()).unwrap(),
+            y: splits.next().and_then(|s| s.trim().parse::<i32>().ok()).unwrap(),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Segment {
+    start: Point,
+    end: Point
+}
+
+impl Segment {
+    pub fn parse_segment(line: &str) -> Segment {
+        let mut splits = line.split("->");
+
+        Segment {
+            start: Point::parse_point(splits.next().unwrap()),
+            end: Point::parse_point(splits.next().unwrap())
+        }
+    }
+
+    pub fn points(&self) -> Vec<Point> {
+        let raw_dx = self.end.x - self.start.x;
+        let raw_dy = self.end.y - self.start.y;
+        let dx = i32::signum(raw_dx);
+        let dy = i32::signum(raw_dy);
+
+        let mut ret = Vec::with_capacity(i32::abs(raw_dx.min(raw_dy)) as usize);
+        let mut cur_point = self.start;
+
+        while cur_point != self.end {
+            ret.push(cur_point);
+            cur_point = Point {
+                x: cur_point.x + dx,
+                y: cur_point.y + dy
+            }
+        }
+        ret.push(self.end);
+
+        ret
+    }
+
+    pub fn is_vertical(&self) -> bool {
+        self.start.x == self.end.x
+    }
+
+    pub fn is_horizontal(&self) -> bool {
+        self.start.y == self.end.y
+    }
+}
+
+pub fn run() {
+    let lines = fileutil::read_lines("data/2021/05.txt").unwrap();
+
+    let segments: Vec<Segment> = lines.into_iter()
+    .map(|s| Segment::parse_segment(&s))
+    .collect();
+
+    let mut points: HashMap<Point, i32> = HashMap::new();
+    for segment in segments {
+        if !segment.is_vertical() && !segment.is_horizontal() {
+            continue;
+        }
+
+        // println!("{:?}", segment);
+        for point in segment.points() {
+            // println!("{:?}", point);
+            let entry = points.entry(point).or_insert(0);
+            *entry += 1;
+        }
+    }
+
+    let ge2_overlaps = points.values().filter(|v| **v >= 2).count();
+
+    println!("{}", ge2_overlaps);
+}
--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,5 @@ mod advent2019;
 mod advent2021;
 
 fn main() -> () {
-    advent2021::day04::run();
+    advent2021::day05::run();
 }
