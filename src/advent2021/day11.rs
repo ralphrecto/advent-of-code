f1ce523d54f2ef37f2cd8079133dbddc89a1656f
Ralph Recto <recto.ralph@gmail.com>
2021-12-11 11:07:07 -0500

    Finish day 11

--- src/advent2021/day11.rs
+++ src/advent2021/day11.rs
@@ -1,5 +1,13 @@
+use std::collections::HashSet;
+
 use fileutil;
 use itertools::Itertools;
+
+#[derive(PartialEq,Eq)]
+enum Mode {
+    NUM_FLASHES,
+    FIRST_SYNC
+}
 
 fn adjacent<'a>(energy_levels: &'a Vec<Vec<u32>>, row: usize, col: usize) -> Box<dyn Iterator<Item=(usize, usize)> + 'a> {
     let iter = (-1..=1).cartesian_product(-1..=1)
@@ -17,54 +25,65 @@ fn adjacent<'a>(energy_levels: &'a Vec<Vec<u32>>, row: usize, col: usize) -> Box
     Box::new(iter)
 }
 
-fn sim(steps: usize) -> u32 {
-    let mut energy_levels: Vec<Vec<u32>> = fileutil::read_lines("data/2021/11-tmp.txt")
+fn sim(steps: usize, mode: Mode) -> u32 {
+    let mut energy_levels: Vec<Vec<u32>> = fileutil::read_lines("data/2021/11.txt")
         .unwrap()
         .iter()
         .map(|s| s.chars().map(|c| c.to_digit(10).unwrap()).collect())
         .collect();
+    let height= energy_levels.len();
+    let width = energy_levels[0].len();
     let mut buffer = energy_levels.clone();
    
-    for level in energy_levels.iter() {
-        println!("{:?}", level);
-    }
-    println!("");
-
-    let mut num_flashes: u32 = 0;
-    for _ in 0..steps {
-
+    let mut num_total_flashes: u32 = 0;
+    for step_num in 1..=steps {
         // Energy increase phase
-        for i in 0..energy_levels.len() {
-            for j in 0..energy_levels[0].len() {
+        for i in 0..height {
+            for j in 0..width {
                 buffer[i][j] = buffer[i][j] + 1;
-
-                if buffer[i][j] > 9 {
-                    for (adj_i, adj_j) in adjacent(&energy_levels, i, j) {
-                        // println!("i {}, j {}, add_i {}, adj_j {}", i, j, adj_i, adj_j);
-                        buffer[adj_i][adj_j] += 1;
-                    }
-                }
             }
         }
 
-        for level in energy_levels.iter() {
-            println!("{:?}", level);
+        // Adjacent cells increase phase.
+        let mut seen: HashSet<(usize, usize)> = HashSet::new();
+        loop {
+            let mut fixed = true;
+            for i in 0..height {
+                for j in 0..width {
+                    if buffer[i][j] > 9 && !seen.contains(&(i, j)) {
+                        seen.insert((i, j));
+                        fixed = false;
+                        for (adj_i, adj_j) in adjacent(&energy_levels, i, j) {
+                            buffer[adj_i][adj_j] += 1;
+                        }
+                    }
+                }
+            }
+
+            if fixed {
+                break;
+            }
         }
-        println!("flash");
 
         // Flash phase
-        for i in 0..energy_levels.len() {
-            for j in 0..energy_levels[0].len() {
+        let mut num_step_flashes: u32 = 0;
+        for i in 0..height {
+            for j in 0..width {
                 if buffer[i][j] > 9 {
-                    num_flashes += 1;
+                    num_total_flashes += 1;
+                    num_step_flashes += 1;
                     buffer[i][j] = 0;
                 }
             }
         }
 
+        if num_step_flashes == (height * width) as u32 && mode == Mode::FIRST_SYNC {
+            return step_num as u32;
+        }
+
         // Copy buffer phase
-        for i in 0..energy_levels.len() {
-            for j in 0..energy_levels[0].len() {
+        for i in 0..height {
+            for j in 0..width {
                 energy_levels[i][j] = buffer[i][j];
             }
         }
@@ -74,19 +93,12 @@ fn sim(steps: usize) -> u32 {
         buffer = tmp;
     }
 
-    for level in energy_levels.iter() {
-        println!("{:?}", level);
+    match mode {
+        Mode::NUM_FLASHES => num_total_flashes,
+        Mode::FIRST_SYNC => panic!("Did not find first sync time")
     }
-    // println!("buffer");
-
-    // for level in buffer.iter() {
-    //     println!("{:?}", level);
-    // }
-    println!("");
-
-    num_flashes
 }
 
 pub fn run() {
-    println!("{}", sim(2));
+    println!("{}", sim(usize::MAX, Mode::FIRST_SYNC));
 }
